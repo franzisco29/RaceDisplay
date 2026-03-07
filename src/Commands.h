6edc75cb499e7429bf5145660ea016d48f45ca89
franzisco29 <127801295+franzisco29@users.noreply.github.com>
2026-03-07 15:19:44 +0100

    Updated

---
+++ src/Commands.h
@@ -0,0 +1,59 @@
+//** COMMANDS */
+#ifndef COMANDI_H
+#define COMANDI_H
+//CMD OF CONNECTION & UTILITIES
+
+#define CONN_CMD 'C'
+#define FAN_CMD 'W'
+#define DSCN_CMD 'c'
+//CMD FOR START
+#define LIGHTS_OUT_CMD 'S'  
+#define START_PROC_CMD 's' 
+
+
+//CMD FOR FLAGS
+#define GREEN_FLAG_CMD 'G'
+#define RED_FLAG_CMD 'R'
+
+#define YELLOW_F_CMD '1'
+#define YELLOW_S_CMD '2'
+#define YELLOW_T_CMD '3'
+#define YELLOW_FS_CMD '4'
+#define YELLOW_ST_CMD '5'
+#define YELLOW_TF_CMD '6'
+
+#define WET_CMD 'W'
+
+//CMD FOR SAFETY
+#define SAFETY_CAR_CMD 'Y'
+#define FULL_YELLOW_CMD 'F'
+
+
+#define PIT_CLOSER_CMD 'P'
+#define PIT_OPEN_CMD 'O'
+#define PIT_VALID_CMD 'V'
+
+#define END_SESSION_CMD 'E'
+
+#define CLC_CMD 'd'
+#define CLC_YELLOW_CMD 'T'
+
+#define PRE_RACE_CMD 'p'
+#define PRE10_CMD '7'
+#define PRE5_CMD '8'
+#define PRE2_CMD  '9'
+#define PRE1_CMD '0'
+#define FORMATION_LAP_CMD 'L'
+
+
+#define BRIEFING_CMD 'b'
+
+
+#define MENU_CMD 'M'
+/// --- MENU ----///
+
+#define STATUS_CMD 'A'
+#define BACK 'B'
+
+ 
+#endif
